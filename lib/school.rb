6e42ac299ac033ec12f81c55304069f0efc0bcc6
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 10:52:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  school = School.new("Bayside High School")
+  school.roster
+  school.add_student("Zach Morris", 9)
+  school.roster
+  hash[]
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
+  school.roster
+
+
+def add_student
+
+end
+
+def grade(grade)
+hash
+end
+
+
+
+school.sort
