abfc416a85950b04b972fd1217780ce2c93822a8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 11:01:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,20 +2,19 @@ class School
 
 
 def school(name)
-  school = School.new("Bayside High School")
+  @name
+  name = School.new("Bayside High School")
 
 school.roster = {}
 end
 
 def add_student(name, grade)
   hash[new_key] = []
-
-  # school.add_student("Zach Morris", 9)
-  # school.roster
-  # hash[]
-  # school.add_student("AC Slater", 9)
-  # school.add_student("Kelly Kapowski", 10)
-  # school.add_student("Screech", 11)
+  
+  school.add_student("Zach Morris", 9)
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
 end
 
 
