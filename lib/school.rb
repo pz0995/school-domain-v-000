b59b02742770748f7e8f6c6c31789835045f4aa6
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 11:21:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,21 +8,18 @@ def school(name)
   @school.roster = {}
 end
 
-def add_student=school.roster(name, grade)
-  grade = Array.new
+def add_student=school(name, grade)
+  school.roster grade = Array.new
   9 << "Zach Morris"
   9 << "AC Slater"
-  10<< ."Kelly Kapowski"
+  10 << "Kelly Kapowski"
   11 << "Screech"
-add_student.sort
-
 
 end
-
-
 
 def grade(grade)
 school.grade(9)
 school.grade(10)
 school.grade(11)
+@school.sort
 end
